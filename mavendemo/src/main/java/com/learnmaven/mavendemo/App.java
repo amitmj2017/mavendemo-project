68910ce52e9cd8a34c077cfd3e1b550a05b02802
amitmj2017 <50351267+amitmj2017@users.noreply.github.com>
2019-05-07 14:43:47 +0530

    Update App.java

--- mavendemo/src/main/java/com/learnmaven/mavendemo/App.java
+++ mavendemo/src/main/java/com/learnmaven/mavendemo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!jksdgasjkghdhasdj" );
+        System.out.println( "Hello dffdfdfdfld!jksdgasjkghdhasdj" );
     }
 }
